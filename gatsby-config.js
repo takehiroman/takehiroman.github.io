3765a4ef7e8d94c58f1ef58cf3db077046cc26f5
tsuzaki takehiro <takehanogi11@tsuzaki-no-MacBook-Air.local>
2018-03-26 21:21:23 +0900

    サイト名変更

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,7 @@ module.exports = {
     description: 'A starter blog demonstrating what Gatsby can do.',
     siteUrl: 'https://gatsbyjs.github.io/gatsby-starter-blog/',
   },
+  pathPrefix: '/Zakimoto-blog',
   plugins: [
     {
       resolve: `gatsby-source-filesystem`,
